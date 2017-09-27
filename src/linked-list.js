766280a2a9331f2d5929d17e7e65ec9b5bfba588
Ekaterina McArenko <polumna-la@mail.ru>
2017-09-27 15:50:53 +0300

    Version1

--- src/linked-list.js
+++ src/linked-list.js
@@ -1,27 +1,162 @@
 const Node = require('./node');
 
 class LinkedList {
-    constructor() {}
+    constructor() {
+        this.length = 0;
+        this._tail = null;
+        this._head = null;
+    }
 
-    append(data) {}
+    append(data) {
 
-    head() {}
 
-    tail() {}
+        var node= new Node(data)
+        if (this._head === null) {
+            this._head = node; 
+            this._tail =  node; 
+            
+            
+        } else {
+            this._tail.next = node;
+            node.prev=this._tail
+            this._tail=node 
+            
+            
+        }
+        this.length++;
+        return this
 
-    at(index) {}
+    }
 
-    insertAt(index, data) {}
+    head() {
+        return this._head.data;
+    }
 
-    isEmpty() {}
+    tail() {
+        return this._tail.data;
+    }
 
-    clear() {}
+    at(index) {
+        var curent=this._head
+        for (var i = 0; i <= index; i++) {
+        	if(index===i){
+        		return curent.data
+        	}else{
+        		curent = curent.next;}
+        }
+    }
 
-    deleteAt(index) {}
 
-    reverse() {}
+    insertAt(index, data) {
 
-    indexOf(data) {}
+        
+    var node=new Node(data)
+    	
+    	 	
+    	if(index==0){
+    		if(!this._head){
+    			this._head = node;
+    			return;
+    		} else {
+    			node.next=this._head;
+    			this._head.prev=node;
+    			this._head=node;
+    			return;
+    		}
+    	} else if (index==this.length){
+	    	this._tail.next=node;
+	    	node.prev=this._tail;
+	    	this._tail=node;
+	    	return;
+    	} else {
+    		var curent = this._head;
+          	for (var i=0; i<index-1; i++){
+        	curent = curent.next;
+    		}
+			node.next=curent.next;
+			node.prev=curent;
+			curent.next=node;
+			node.next.prev=node;
+		}
+		this.length++;
+		return this;
+			    
+
+    }
+
+    isEmpty() {
+        if (this.length==0){
+            return true
+        } else {
+            return false
+        }
+    
+    }
+
+    clear() {
+        this._head.data=null;
+        this._tail.data=null;
+        this.length=0;
+        return this
+    }
+
+    deleteAt(index) {
+        var curent
+
+        if (index == 0) {
+        this._head=this._head.next
+        this.length--
+    	}
+
+        if (index < this.length-1){
+        	curent=this._head
+            for(var i=0; i<index-1; i++)
+            {curent=curent.next}}
+
+            curent.next=curent.next.next
+            curent.next.next.prev=curent
+            this.length--
+            return this
+    
+    
+    }
+
+    reverse() {
+    	 
+    	
+    	var curent=this._head
+    	for(var i=0; i<this.length; i++){
+    		
+    		var tmp=curent.prev
+    		curent.prev=curent.next
+    		curent.next=tmp
+    		curent=curent.prev
+    	}
+
+    	var cur=this._tail
+    	this._tail=this._head
+    	this._head=cur
+    	return this;
+
+    }
+
+    indexOf(data) {
+    	var curent=this._head
+    
+	    if (this.length>1){
+	    	for(var i=0; i<this.length; i++){
+	    		if(curent.data===data){
+	    			return i
+	    		} else {
+	    			curent=curent.next
+	    		}
+
+	    	}
+	    }	 	
+	 	return -1
+	}
+
+	
 }
 
 module.exports = LinkedList;
